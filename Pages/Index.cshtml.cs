9c3c8df76a307b7eb581dece36a7089112414608
Mitren Chinoy <michinoy@microsoft.com>
2020-05-04 13:25:11 -0700

    test application code

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace github_action_testapp_dotnetcore.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
